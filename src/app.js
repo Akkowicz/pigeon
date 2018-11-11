9fe6677035b0e790a71f02fbe7777a034114fccc
Mateusz Przybyłowicz <uamfhq@gmail.com>
2018-11-11 22:01:42 +0100

    Sync with local

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 import { kuik } from './libkuik';
+import { ui } from './ui';
+import { getPriority } from 'os';
 
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for post submit
+ui.postSubmit.addEventListener('click', submitPost);
+
+// Get posts
 function getPosts() {
     kuik.get('http://localhost:3000/posts')
-        .then(data => console.log(data))
+        .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}
+
+// Submit post
+function submitPost() {
+    const body = ui.bodyInput.value;
+
+    const data = {
+        body
+    };
+
+    // Create Post
+
+    kuik.post('http://localhost:3000/posts', data)
+        .then(data => {
+            ui.showAlert('Post added', 'alert alert-success');
+            getPosts();
+        })
         .catch(err => console.log(err));
 }
--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,46 @@
 class UI {
     constructor() {
         this.post = document.querySelector('#posts');
-        this.titleInput = document.querySelector('#body');
+        this.bodyInput = document.querySelector('#body');
+        this.idInput = document.querySelector('#id');
+        this.postSubmit = document.querySelector('.post-submit');
+        this.forState = 'add';
+    }
+
+    showPosts(posts) {
+        let output = '';
+
+        posts.forEach((post) => {
+            output += `
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <p class="card-text">${post.body}</p>
+                        <a href="#" class="edit card-link" data-id="${post.id}">
+                            <i class="fa fa-pencil"></i>
+                        </a>
+                        <a href="#" class="delete card-link" data-id="${post.id}">
+                            <i class="fa fa-remove"></i>
+                        </a>
+
+                    </div>
+                </div>
+            `;
+        });
+        
+        this.post.innerHTML = output;
+    }
+
+    showAlert() {
+
+    }
+
+    clearAlert() {
+
+    }
+
+    clearFields() {
+        this.bodyInput.value = '';
     }
 }
+
+export const ui = new UI;
